1947522acbdd90606012fb0761d2d69d7a2772ae
dphoeng <329163@student.mboutrecht.nl>
2023-06-20 16:51:27 +0200

    Added sp and vw to db file

--- db/db.sql
+++ db/db.sql
@@ -480,3 +480,282 @@ begin
 end //
 DELIMITER ;
 
+
+DROP PROCEDURE IF EXISTS spCreateVoedselpakket;
+
+DELIMITER //
+    
+CREATE PROCEDURE spCreateVoedselpakket
+(
+    ToSplit VARCHAR(1000),
+    _klantId INT
+)
+
+BEGIN
+
+	DECLARE Remainder TEXT;
+	DECLARE Pos INT DEFAULT 1;
+	DECLARE Str VARCHAR(1000);
+    DECLARE SubPos INT DEFAULT 1;
+    DECLARE Aantal INT UNSIGNED DEFAULT 0;
+	DECLARE ProductId INT UNSIGNED DEFAULT 0;
+	DECLARE VoedselpakketId INT UNSIGNED DEFAULT 0;
+    
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		ROLLBACK;
+		SELECT 'An error has occurred, operation rollbacked and the stored procedure was terminated';
+    END;
+    
+    START TRANSACTION;
+		SET Remainder = ToSplit;
+        
+        INSERT INTO `Voedselpakket` (`KlantId`, `DatumSamenstelling`) VALUES (_klantId, CURRENT_TIMESTAMP);
+		SET VoedselpakketId = last_insert_id();
+
+		WHILE CHAR_LENGTH(Remainder) > 0 AND Pos > 0 DO
+			SET Pos = INSTR(Remainder, ',');
+			IF Pos = 0 THEN
+				SET Str = Remainder;
+			ELSE
+				SET Str = LEFT(Remainder, Pos - 1);
+			END IF;
+			IF TRIM(Str) != '' THEN
+				SET SubPos = INSTR(Str, ':');
+                SET ProductId = CAST(LEFT(Str, SubPos - 1) AS UNSIGNED);
+                SET Aantal = CAST(SUBSTRING(Str, SubPos + 1, CHAR_LENGTH(Str) - SubPos) AS UNSIGNED);
+				INSERT INTO `VoedselpakketProduct` (
+					 `VoedselpakketId`
+                    ,`ProductId`
+                    ,`Aantal`
+                    ) 
+				VALUES (
+					 VoedselpakketId
+                    ,ProductId
+                    ,Aantal
+				);
+				-- UPDATE `Product` 
+                -- SET `AantalInVoorraad` = `AantalInVoorraad` - Aantal
+                -- WHERE `Id` = ProductId;
+			END IF;
+
+			SET Remainder = SUBSTRING(Remainder, Pos + 1);
+		END WHILE;
+        COMMIT;
+END; //
+
+DROP PROCEDURE IF EXISTS spDeleteVoedselpakket;
+
+DELIMITER //
+    
+CREATE PROCEDURE spDeleteVoedselpakket
+(
+	_id	INT
+)
+
+BEGIN
+
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+    	ROLLBACK;
+    	SELECT 'An error has occurred, operation rollbacked and the stored procedure was terminated';
+	END;
+            
+	START TRANSACTION;
+        DELETE FROM `VoedselpakketProduct` WHERE VoedselpakketId = _id;
+        DELETE FROM `Voedselpakket` WHERE Id = _id;
+        COMMIT;
+END //
+
+DROP PROCEDURE IF EXISTS spGetAllergieenPerKlantId;
+
+DELIMITER //
+    
+CREATE PROCEDURE spGetAllergieenPerKlantId
+(
+	_klantId	INT
+)
+
+BEGIN
+
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+    	ROLLBACK;
+    	SELECT 'An error has occurred, operation rollbacked and the stored procedure was terminated';
+	END;
+            
+	START TRANSACTION;
+        SELECT ale.Naam FROM `Klant` kla INNER JOIN `KlantAllergie` kal ON kal.KlantId = kla.Id INNER JOIN `Allergie` ale ON ale.Id = kal.AllergieId WHERE kla.Id = _klantId;
+        COMMIT;	
+END //
+
+DROP PROCEDURE IF EXISTS spGetAllProductenByVoedselpakketId;
+
+DELIMITER //
+    
+CREATE PROCEDURE spGetAllProductenByVoedselpakketId
+(
+	_id	INT
+)
+
+BEGIN
+
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+    	ROLLBACK;
+    	SELECT 'An error has occurred, operation rollbacked and the stored procedure was terminated';
+	END;
+            
+	START TRANSACTION;
+        SELECT 
+            vpw.Aantal,
+            pro.AantalInVoorraad,
+            pro.ProductNaam, 
+            cat.Naam, 
+            Pro.Id 
+        FROM (SELECT 
+                * 
+            FROM VoedselpakketProduct vpr 
+            WHERE vpr.VoedselpakketId = _id
+        ) vpw 
+        RIGHT JOIN Product pro 
+        ON vpw.ProductId = pro.Id 
+        INNER JOIN Categorie cat 
+        ON pro.CategorieId = cat.Id;
+END //
+
+DROP PROCEDURE IF EXISTS spGetKlantPerId;
+
+DELIMITER //
+    
+CREATE PROCEDURE spGetKlantPerId
+(
+	_klantId	INT
+)
+
+BEGIN
+
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+    	ROLLBACK;
+    	SELECT 'An error has occurred, operation rollbacked and the stored procedure was terminated';
+	END;
+            
+	START TRANSACTION;
+        SELECT Id, Voornaam, Tussenvoegsel, Achternaam, AantalVolwassenen, AantalKinderen, AantalBabys FROM `Klant` WHERE Id = _klantId;
+        COMMIT;	
+END //
+
+DROP PROCEDURE IF EXISTS spGetProductById;
+
+DELIMITER //
+    
+CREATE PROCEDURE spGetProductById
+(
+	_productId	INT
+)
+
+BEGIN
+
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+    	ROLLBACK;
+    	SELECT 'An error has occurred, operation rollbacked and the stored procedure was terminated';
+	END;
+            
+	START TRANSACTION;
+        SELECT `AantalInVoorraad` FROM `Product` WHERE `Id` = _productId;
+END //
+
+
+DROP PROCEDURE IF EXISTS spGetProductenByVoedselpakketId;
+
+DELIMITER //
+    
+CREATE PROCEDURE spGetProductenByVoedselpakketId
+(
+	_id	INT
+)
+
+BEGIN
+
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+    	ROLLBACK;
+    	SELECT 'An error has occurred, operation rollbacked and the stored procedure was terminated';
+	END;
+            
+	START TRANSACTION;
+        SELECT vpr.Aantal, pro.ProductNaam, pro.AantalInVoorraad, cat.Naam FROM `VoedselpakketProduct` vpr INNER JOIN `Product` pro ON pro.Id = vpr.ProductId INNER JOIN `Categorie` cat ON cat.Id = pro.CategorieId WHERE vpr.VoedselpakketId = _id;
+END //
+
+
+DROP PROCEDURE IF EXISTS spGetVoedselpakketById;
+
+DELIMITER //
+    
+CREATE PROCEDURE spGetVoedselpakketById
+(
+	_id	INT
+)
+
+BEGIN
+
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+    	ROLLBACK;
+    	SELECT 'An error has occurred, operation rollbacked and the stored procedure was terminated';
+	END;
+            
+	START TRANSACTION;
+        SELECT DatumSamenstelling, DatumUitgifte, KlantId FROM `Voedselpakket` WHERE Id = _id;
+END //
+
+
+
+DROP PROCEDURE IF EXISTS spGetWensenPerKlantId;
+
+DELIMITER //
+    
+CREATE PROCEDURE spGetWensenPerKlantId
+(
+	_klantId	INT
+)
+
+BEGIN
+
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+    	ROLLBACK;
+    	SELECT 'An error has occurred, operation rollbacked and the stored procedure was terminated';
+	END;
+            
+	START TRANSACTION;
+        SELECT wen.Naam FROM `Klant` kla  INNER JOIN `KlantWens` kwe ON kwe.KlantId = kla.Id INNER JOIN `Wens` wen ON wen.Id = kwe.WensId WHERE kla.Id = _klantId;
+        COMMIT;	
+END //
+
+DROP PROCEDURE IF EXISTS spUpdateVoedselpakket;
+
+DELIMITER //
+    
+CREATE PROCEDURE spUpdateVoedselpakket
+(
+    _date datetime,
+    _id int
+)
+
+BEGIN
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		ROLLBACK;
+		SELECT 'An error has occurred, operation rollbacked and the stored procedure was terminated';
+    END;
+    
+    START TRANSACTION;
+        UPDATE `Voedselpakket` SET `DatumUitgifte` = _date WHERE `Id` = _id;
+        COMMIT;
+END; //
+
+CREATE VIEW vwGetAvailableProducten AS
+SELECT pro.Id, pro.ProductNaam, pro.AantalInVoorraad, cat.Naam  FROM `Product` pro INNER JOIN `Categorie` cat ON cat.Id = pro.CategorieId WHERE pro.AantalInVoorraad > 0;
