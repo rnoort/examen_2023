11b66663c9ab0b27ffe27930870629e1babc323c
Robijn Noort <rrnoort@gmail.com>
2023-06-20 10:02:05 +0200

    feat: added overview for leveranciers

---
+++ app/views/leveranciers/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leveranciers</title>
+    <!-- Tailwind -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <header class="flex justify-between p-4">
+        <h1 class="font-bold text-3xl">Leveranciers</h1>
+        <a class="bg-green-400 hover:bg-green-500 font-bold px-4 py-2 rounded" href="<?= URLROOT . "/leverancier/create"?>">Toevoegen</a>
+    </header>
+
+    <div class="p-4">
+        <table class="w-full text-sm text-left text-gray-500">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                <tr>
+                    <th scope="col">Bedrijfsnaam</th>
+                    <th scope="col">Adres</th>
+                    <th scope="col">Contact</th>
+                    <th scope="col">Eerst volgende levering</th>
+                    <th scope="col">Acties</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($data["records"] as $record): ?>
+                    <tr>
+                        <td><?= $record->Bedrijfsnaam?></td>
+                        <td><?= $record->Straatnaam . " " . $record->Huisnummer . " " . $record->Toevoeging . " " . $record->Postcode . " " . $record->Plaats ?></td>
+                        <td><pre><?= $record->ContactNaam . $record->Email . $record->Telefoonnummer ?></pre></td>
+                        <td><?= $record->EerstVolgendeLevering ?></td>
+
+                        <td><a class="bg-orange-400 hover:bg-orange-500 font-bold px-4 py-2 rounded" href="<?= URLROOT . '/leverancier/update/' . $record->Id?>">Wijzigen</a></td>
+                        <td><a class="bg-red-400 hover:bg-red-500 font-bold px-4 py-2 rounded" href="<?= URLROOT . '/leverancier/delete/' . $record->Id?>">Verwijderen</a></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
